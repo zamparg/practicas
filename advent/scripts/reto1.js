c13845b8a05d5e5449969de6a9cb8b9a75507b45
zamparg <zamparg@hotmail.com>
2023-04-08 13:00:09 -0300

    boton ver mas

--- advent/scripts/reto1.js
+++ advent/scripts/reto1.js
@@ -1,27 +1,17 @@
-const codigo = document.getElementById("reto1");
-const contenido = document.getElementById("contenido1");
-document.getElementById("reto1Content").addEventListener("click", mostrar())
+const contenido = document.getElementById("hide1");
+const boton = document.getElementById("change1")
+boton.addEventListener("click", change)
 
-function mostrar(){
-    contenido.innerHTML=`
-    <p>Este año los elfos han comprado una máquina que envuelve regalos. Pero… ¡no viene programada! Necesitamos crear un algoritmo que le ayude en la tarea.
-
-    A la máquina se le pasa un array con los regalos. Cada regalo es un string. Necesitamos que la máquina envuelva cada regalo en papel de regalo y lo coloque en un array de regalos envueltos.
-    
-    El papel de regalo es el símbolo * y para envolver un regalo se coloca el símbolo * de forma que rodee totalmente al string por todos los lados. Por ejemplo:</p>
-    `
+function change(){
+    contenido.classList.toggle('hide')
+    if(boton.textContent=='Ver más'){
+        boton.textContent= 'Ver Menos'
+    } else{
+        boton.textContent= 'Ver Más'
+    }
 }
-codigo.textContent = `
 
-function wrapping(gifts) {
-    let wrap = gifts.map((gift) =>
-     
-      ('*'.repeat(gift.length+2)) + '\\n*'+gift+'*\\n'+('*'.repeat(gift.length+2))
-      )
-  
-    return wrap
-  }
-`
+
 
 function wrapping(gifts) {
 
