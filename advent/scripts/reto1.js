e9ddfd08a0c0fc99d4f26c53458b7956f3006cc1
zamparg <zamparg@hotmail.com>
2023-04-17 12:58:50 -0300

    ejercicio1

--- advent/scripts/reto1.js
+++ advent/scripts/reto1.js
@@ -2,8 +2,12 @@ const contenido = document.getElementById("hide1");
 const boton = document.getElementById("change1")
 boton.addEventListener("click", change)
 
+const codigo = document.getElementById("code1");
+const botonCodigo = document.getElementById("view1")
+botonCodigo.addEventListener("click", changeCodigo)
+
 const botonTest = document.getElementById("test1")
-botonTest.addEventListener("click", change)
+botonTest.addEventListener("click", test)
 
 
 
@@ -16,6 +20,30 @@ function change(){
     }
 }
 
+function changeCodigo(){
+    codigo.classList.toggle('hide')
+    if(botonCodigo.textContent=='Ver Código'){
+        botonCodigo.textContent= 'Ocultar Código'
+    } else{
+        botonCodigo.textContent= 'Ver Código'
+    }
+}
+
+function test(){
+    let ingress = document.getElementById("input1").value.replace(/ /g, "")
+    let tester = ingress.split(',')
+    let result= wrapping(tester)
+    console.log(result)
+    let screen=document.getElementById('screen1')
+    screen.innerHTML=``
+
+    result.forEach(element => {
+        screen.innerHTML+=`
+        <p class="text-center">${element}</p>
+        `
+    });
+
+}
 
 
 function wrapping(gifts) {
